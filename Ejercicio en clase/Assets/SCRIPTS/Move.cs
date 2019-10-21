0dd8070f9f27914a0aa92317ee632a1995881094
jjmarin20 <55495486+jjmarin20@users.noreply.github.com>
2019-10-21 09:02:40 -0500

    MElo

---
+++ Ejercicio en clase/Assets/SCRIPTS/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public float movSpeed = 5F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(transform.right* movSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(transform.right * -1 * movSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(transform.forward * movSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(transform.forward * -1 * movSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(transform.up * movSpeed * Time.deltaTime);
+        }
+    }
+
+}
+
